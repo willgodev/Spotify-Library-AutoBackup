02850fd0bfba55ee40921d5972a3ad9c31ce3c51
wgoins6 <wgoins6@gatech.edu>
2022-02-12 19:21:14 -0500

    Add batch file for task scheduler to autorun daily

---
+++ Run_Spotify_Backup.bat
@@ -0,0 +1,5 @@
+call Q:\miniconda3\Scripts\activate.bat "C:\Users\willi\anaconda3\envs\Liked_Song_Synchronizer"
+
+python "Q:\workspace\Spotify-Library-AutoBackup\Liked_Song_Synchronizer.py"
+
+pause
